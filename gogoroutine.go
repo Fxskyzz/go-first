db8e092665caa108c220cddbe75403ecf54f53c3
felix <amarezz@163.com>
2019-09-10 11:53:04 +0800

    go通道

--- gogoroutine.go
+++ gogoroutine.go
@@ -11,7 +11,7 @@ go 函数名(参数列表)
 **/
 
 // 实例
-
+/*
 package main
 
 import "fmt"
@@ -29,3 +29,46 @@ func main()  {
 	go say("world")
 	say("go")
 }
+**/
+
+// go通道
+/*
+通道（channel）是用来传递数据的一个数据结构。
+通道可用于两个 goroutine 之间通过传递一个指定类型的值来同步运行和通讯。操作符 <- 用于指定通道的方向，发送或接收。如果未指定方向，则为双向通道。
+ch <- v // 把v发送到通道ch
+v := <-ch // 从ch接收数据并把值赋值给v
+**/
+
+/*
+声明一个通道很简单，我们使用chan关键字即可，通道在使用前必须先创建
+ch := make(chan, int)
+注意：默认情况下，通道是不带缓冲区的。发送端发送数据，同时必须又接收端相应的接收数据。
+以下实例通过两个 goroutine 来计算数字之和，在 goroutine 完成计算后，它会计算两个结果的和：
+**/
+
+// 实例
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int){
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // 把sum发送到通道c
+}
+
+func main()  {
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // 从通道c中接收
+
+	fmt.Println(x, y, x+y)
+}
+
+
+
+
